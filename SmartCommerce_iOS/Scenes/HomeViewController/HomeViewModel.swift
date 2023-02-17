91d4e13d0000d72a2704af7ecf7edfea6fc0a235
tjdrb3807 <junsung3807@naver.com>
2023-02-17 11:22:04 +0900

    feat: create HomeViewModel

---
+++ SmartCommerce_iOS/Scenes/HomeViewController/HomeViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewModel.swift
+//  SmartCommerce_iOS
+//
+//  Created by 전성규 on 2023/02/16.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+struct HomeViewModel {
+    let menuSectionViewModel = MenuSectionViewModel()
+    
+    init() {
+    }
+}
