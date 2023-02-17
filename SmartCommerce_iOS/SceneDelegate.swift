c9616230c352e33b2da9a8587cb5f91bc19e91e2
tjdrb3807 <junsung3807@naver.com>
2023-02-17 11:24:09 +0900

    feat: bind homeViewModel

--- SmartCommerce_iOS/SceneDelegate.swift
+++ SmartCommerce_iOS/SceneDelegate.swift
@@ -10,12 +10,15 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    let homeViewModel = HomeViewModel()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
         self.window = UIWindow(windowScene: windowScene)
         
         let rootViewController = HomeViewController()
+        rootViewController.bind(homeViewModel)
+        
         self.window?.rootViewController = rootViewController
         self.window?.backgroundColor = .white
         self.window?.makeKeyAndVisible()
