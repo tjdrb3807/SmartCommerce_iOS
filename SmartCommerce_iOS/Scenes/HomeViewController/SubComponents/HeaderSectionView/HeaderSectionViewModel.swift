913624673ad6b37657ab7256d2e61a9d53ca3919
tjdrb3807 <junsung3807@naver.com>
2023-02-19 18:11:58 +0900

    feat: create HeaderSectionViewModel

---
+++ SmartCommerce_iOS/Scenes/HomeViewController/SubComponents/HeaderSectionView/HeaderSectionViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  HeaderSectionViewModel.swift
+//  SmartCommerce_iOS
+//
+//  Created by 전성규 on 2023/02/17.
+//
+
+import RxSwift
+import RxCocoa
+
+struct HeaderSectionViewModel {
+    
+    init() {
+        
+    }
+}
