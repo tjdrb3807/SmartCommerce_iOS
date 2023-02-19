8d743935df76aa905eb4d80a87b40009be9195c5
tjdrb3807 <junsung3807@naver.com>
2023-02-19 18:09:13 +0900

    feat: create RecommendViewController

---
+++ SmartCommerce_iOS/Scenes/HomeViewController/SubComponents/RecommendViewController/RecommendViewController.swift
@@ -0,0 +1,38 @@
+//
+//  RecommendViewController.swift
+//  SmartCommerce_iOS
+//
+//  Created by 전성규 on 2023/02/19.
+//
+
+import UIKit
+import SnapKit
+
+final class RecommendViewController: UIViewController {
+    private lazy var sampleTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Recommend View"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 20.0, weight: .bold)
+        
+        return label
+    }()
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        self.layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        view.addSubview(sampleTitleLabel)
+        
+        sampleTitleLabel.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
