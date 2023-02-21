77ebcd8d60ab699aa9f4c43338b20b506caa280d
tjdrb3807 <junsung3807@naver.com>
2023-02-21 16:14:27 +0900

    feat: create EventViewController

---
+++ SmartCommerce_iOS/Scenes/HomeViewController/SubComponents/EventViewController/EventViewController.swift
@@ -0,0 +1,38 @@
+//
+//  EventViewController.swift
+//  SmartCommerce_iOS
+//
+//  Created by 전성규 on 2023/02/19.
+//
+
+import UIKit
+import SnapKit
+
+final class EventViewController: UIViewController {
+    private lazy var sampleTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Event View"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 20.0, weight: .bold)
+        
+        return label
+    }()
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        self.layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        view.addSubview(sampleTitleLabel)
+        
+        sampleTitleLabel.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
