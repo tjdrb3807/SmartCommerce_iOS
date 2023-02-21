1437d705d88d93eac85fc1331549835f38dd498d
tjdrb3807 <junsung3807@naver.com>
2023-02-21 20:08:26 +0900

    refactor: ReccommendVidController에  BannerPageViewController 추가

--- SmartCommerce_iOS/Scenes/HomeViewController/SubComponents/RecommendViewController/RecommendViewController.swift
+++ SmartCommerce_iOS/Scenes/HomeViewController/SubComponents/RecommendViewController/RecommendViewController.swift
@@ -7,24 +7,26 @@
 
 import UIKit
 import SnapKit
+import SwiftUI
 
 final class RecommendViewController: UIViewController {
-    private lazy var sampleTitleLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Recommend View"
-        label.textColor = .black
-        label.font = .systemFont(ofSize: 20.0, weight: .bold)
-        
-        return label
-    }()
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
+    private let verticalStackView = UIStackView()
     
-    override func viewDidLoad() {
-        view.backgroundColor = .red
-    }
+    private let bannerContainerView = UIView()
+    private let bannerPageViewController = BannerPageViewController(transitionStyle: .scroll,
+                                                                    navigationOrientation: .horizontal)
+    
+    private let bannerPageControl = BannerPageControl()
+    
+    private let spacingView = UIView()
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         
+        self.addContentView()
+        self.attribute()
         self.layout()
     }
     
@@ -32,11 +34,73 @@ final class RecommendViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func attribute() {
+        verticalStackView.axis = .vertical
+        verticalStackView.distribution = .equalSpacing
+        verticalStackView.spacing = 0.0
+    }
+    
     private func layout() {
-        view.addSubview(sampleTitleLabel)
-        
-        sampleTitleLabel.snp.makeConstraints {
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
+        
+        scrollView.addSubview(contentView)
+        contentView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.width.equalToSuperview()
+        }
+        
+        contentView.addSubview(verticalStackView)
+        verticalStackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        [bannerContainerView, spacingView].forEach { verticalStackView.addArrangedSubview($0) }
+        
+        bannerContainerView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(UIScreen.main.bounds.height / 2)
+        }
+        
+        bannerContainerView.addSubview(bannerPageControl)
+        
+        bannerPageControl.snp.makeConstraints {
+            $0.trailing.equalToSuperview()
+            $0.bottom.equalTo(bannerContainerView.snp.bottom)
+            
+        }
+        
+        spacingView.backgroundColor = .lightGray
+        spacingView.snp.makeConstraints {
+            $0.top.equalTo(bannerContainerView.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(400.0)
+        }
+    }
+    
+    private func addContentView() {
+        addChild(bannerPageViewController)
+        bannerPageViewController.view.frame = bannerContainerView.frame
+        bannerContainerView.addSubview(bannerPageViewController.view)
+        bannerPageViewController.didMove(toParent: self)
+    }
+}
+
+struct RecommendViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        Container()
+    }
+    
+    struct Container: UIViewControllerRepresentable {
+        func makeUIViewController(context: Context) -> UIViewController {
+            RecommendViewController()
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+        
+        typealias UIViewControllerType = UIViewController
     }
 }
