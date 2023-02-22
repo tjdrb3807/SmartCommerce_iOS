799859adfb66902799c1979f0c8b5d5543d565d2
tjdrb3807 <junsung3807@naver.com>
2023-02-22 16:59:37 +0900

    feat: create TabBarViewModel

---
+++ SmartCommerce_iOS/Scenes/TabBarController/TabBarViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarViewModel.swift
+//  SmartCommerce_iOS
+//
+//  Created by 전성규 on 2023/02/22.
+//
+
+import RxSwift
+import RxCocoa
+
+struct TabBarViewModel {
+    // Sub Components
+    let homeViewModel = HomeViewModel()
+    
+    init() {
+        
+    }
+}
