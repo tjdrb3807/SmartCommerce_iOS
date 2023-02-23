78df7a469c72d0594f9c8184594cb76d82cd48e7
tjdrb3807 <junsung3807@naver.com>
2023-02-23 18:10:26 +0900

    ...

--- SmartCommerce_iOS/Entities/ItemTypeData.swift
+++ SmartCommerce_iOS/Entities/ItemTypeData.swift
@@ -1,8 +1,12 @@
 //
-//  ItemTypeTableViewCell.swift
+//  ItemTypeData.swift
 //  SmartCommerce_iOS
 //
 //  Created by 전성규 on 2023/02/23.
 //
 
 import Foundation
+
+struct ItemTypeData {
+    let categoryTitle: String
+}
--- SmartCommerce_iOS/Scenes/SubComponents/CategoryViewController/SubComponents/ItemTypeTableView/ItemTypeTableView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ItemTypeTableView.swift
-//  SmartCommerce_iOS
-//
-//  Created by 전성규 on 2023/02/23.
-//
-
-import Foundation
