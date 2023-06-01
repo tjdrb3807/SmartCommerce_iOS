5c3f7a70ceb1ffd205bd08b6753a4d049a233571
Jeon Seong Gyu <80459245+tjdrb3807@users.noreply.github.com>
2023-06-01 14:32:24 +0900

    ...

--- SmartCommerce_iOS/Scenes/SubComponents/CategoryViewController/SubComponents/EventCategoryListView/EventCategoryListView.swift
+++ SmartCommerce_iOS/Scenes/SubComponents/CategoryViewController/SubComponents/EventCategoryListView/EventCategoryListView.swift
@@ -47,7 +47,6 @@ final class EventCategoryListView: UIView {
                 cell.setData(data)
                 
                 return cell
-                
             }.disposed(by: disposeBag)
     }
     
